ffcb9ea1625a76bd66852a20aef6064fad6920d5
TUDOR-RAREȘ COBORZAN <tudor.coborzan@stud.ubbcluj.ro>
2024-11-06 14:23:51 -0800

    Modificari Lab3

--- Models/Book.cs
+++ Models/Book.cs
@@ -23,5 +23,9 @@ namespace Coborzan_Tudor_Lab2.Models
 
         public int? AuthorID { get; set; }
         public Author? Author { get; set; }
+
+        public ICollection<BookCategory>? BookCategories { get; set; }
+
+    
     }
 }
